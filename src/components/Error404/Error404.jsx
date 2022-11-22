9a2bb76e16bb76aa36895bfb83f4c99297e6b228
NavarroSofiar <navarro.sofiar@gmail.com>
2022-11-22 00:32:15 -0300

    error404

--- src/components/Error404/Error404.jsx
+++ src/components/Error404/Error404.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import mundito from '../Error404/img/Mundo_hecho_de_Banderas.gif'
+//import mundito from '../Error404/img/Mundo_hecho_de_Banderas.gif'
 
 const Error404 = () => {
   return (
@@ -9,7 +9,7 @@ const Error404 = () => {
       <h1>PAGE NOT FOUND</h1>
       <h2>error 404</h2>
       <div><Link to='/home'><button className='btnReload'>Back to countries</button></Link></div>
-      <img src={mundito} />
+     
       
       
        </div>
